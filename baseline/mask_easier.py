52e28aee3ab7947a4244f432bb540b7080380bc4
YEUNU <grinteacake@naver.com>
2022-12-12 22:01:57 +0900

    Initial commit

---
+++ baseline/mask_easier.py
@@ -0,0 +1,25 @@
+import cv2
+import os
+import sys
+import numpy as np
+from tqdm import tqdm
+raw_path = os.path.join(os.getcwd(),"results","pred","gen_20221204_115559","mask")
+
+new_path = os.path.join(os.getcwd(),"results","pred","gen_20221204_115559","new_mask")
+
+os.makedirs(new_path, exist_ok=True)
+
+raw_list = os.listdir(raw_path)
+
+for i in tqdm(range(len(raw_list))):
+    data = cv2.imread(os.path.join(raw_path,raw_list[i]),cv2.IMREAD_GRAYSCALE)
+    sep1,sep2,sep3 = data, data, data
+    sep1 = np.where(sep1 == 1, 255, 0)
+    sep2 = np.where(sep2 == 2, 255, 0)
+    sep3 = np.where(sep3 == 3, 255, 0)
+
+    new_data = np.dstack([sep1,sep2,sep3])
+
+    cv2.imwrite(os.path.join(new_path,raw_list[i]),new_data)
+
+
